f4e0ef5f6368edd58cc0ae49909b7e589e2c9079
Isfar Baset <iib2@georgetown.edu>
2024-10-22 23:39:18 -0400

    Add conf.py for Sphinx documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+project = 'Duplicate Detection Using Bloom Filters and Locality Sensitive Hashing (LSH)'
+copyright = '2024, Isfar Baset, Bella Shi, Sheeba Moghal, Jacky Zhang, Ziyan Di'
+author = 'Isfar Baset, Bella Shi, Sheeba Moghal, Jacky Zhang, Ziyan Di'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. Duplicate Detection Using Bloom Filters and Locality Sensitive Hashing (LSH) documentation master file, created by
+   sphinx-quickstart on Tue Oct 22 23:36:25 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Duplicate Detection Using Bloom Filters and Locality Sensitive Hashing (LSH) documentation
+==========================================================================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
