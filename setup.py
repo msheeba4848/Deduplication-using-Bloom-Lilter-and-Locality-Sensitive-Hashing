7b06d633b1ce8fabd7aba09e91d124ace12078cd
jackyzhang1008 <100536935+jackyzhang1008@users.noreply.github.com>
2024-10-28 21:25:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="yourpackage",
+    name="sweetgreen",
     version="0.1.0",
     packages=find_packages(),
     entry_points={
@@ -10,11 +10,45 @@ setup(
         ]
     },
     install_requires=[
-        # Add any dependencies required for your package here, e.g.:
-        # 'numpy', 'scipy'
+    "alabaster==1.0.0",
+    "babel==2.16.0",
+    "certifi==2024.8.30",
+    "charset-normalizer==3.4.0",
+    "docutils==0.21.2",
+    "idna==3.10",
+    "imagesize==1.4.1",
+    "iniconfig==2.0.0",
+    "Jinja2==3.1.4",
+    "joblib==1.4.2",
+    "MarkupSafe==3.0.2",
+    "networkx==3.4.1",
+    "numpy==2.1.2",
+    "packaging==24.1",
+    "pandas==2.2.3",
+    "pluggy==1.5.0",
+    "Pygments==2.18.0",
+    "pytest==8.3.3",
+    "python-dateutil==2.9.0.post0",
+    "pytz==2024.2",
+    "requests==2.32.3",
+    "scikit-learn==1.5.2",
+    "scipy==1.14.1",
+    "six==1.16.0",
+    "snowballstemmer==2.2.0",
+    "Sphinx==8.1.3",
+    "sphinxcontrib-applehelp==2.0.0",
+    "sphinxcontrib-devhelp==2.0.0",
+    "sphinxcontrib-htmlhelp==2.1.0",
+    "sphinxcontrib-jsmath==1.0.1",
+    "sphinxcontrib-qthelp==2.0.0",
+    "sphinxcontrib-serializinghtml==2.0.0",
+    "threadpoolctl==3.5.0",
+    "tzdata==2024.2",
+    "urllib3==2.2.3",
+    "bitarray==2.3.4"
     ],
     python_requires=">=3.7",
-    author="Your Name",
+    author="Isfar Baset, Bella Shi, Sheeba Moghal, Jacky Zhang, Ziyan Di",
     author_email="your.email@example.com",
     description="A package for Bloom Filter and LSH experiments",
     long_description=open("README.md").read(),
